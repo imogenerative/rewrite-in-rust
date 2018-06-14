1cad3aedcab0147022e56f35bc398a4704e37f3b
Imogen Fulton <imogen@dfdt.me>
2018-06-14 20:10:10 +1000

    oh yeah tests

--- assig_two/src/solution.rs
+++ assig_two/src/solution.rs
@@ -1,5 +1,6 @@
 use time::Time;
 
+#[derive (Copy, Clone)]
 pub struct Solution {
     ftime: Time,
     degree: i32,
@@ -47,5 +48,29 @@ impl Solution {
 
     pub fn set_y(&mut self, y: f32) {
         self.y = y
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Solution;
+    use super::Time;
+
+    #[test]
+    fn solution_tests() {
+        let mut time = Time::new();
+        let mut solution = Solution::new(time, 0, 0.0, 0.0);
+
+        let mut second_time = Time::new();
+        second_time.assign(12, 0, 0);
+        solution.set_time(second_time);
+        solution.set_degree(180);
+        solution.set_x(1.0);
+        solution.set_y(2.0);
+
+        assert_eq!(solution.get_time().secs_since_midnight(), 43200);
+        assert_eq!(solution.get_degree(), 180);
+        assert_eq!(solution.get_x(), 1.0);
+        assert_eq!(solution.get_y(), 2.0);
     }
 }
--- assig_two/src/time.rs
+++ assig_two/src/time.rs
@@ -1,3 +1,4 @@
+#[derive(Copy, Clone)]
 pub struct Time {
     pub hour: i32,
     pub minute: i32,
@@ -67,5 +68,44 @@ impl Time {
 
     pub fn equals(self, t: Time) -> bool {
         self.secs_since_midnight() == t.secs_since_midnight()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Time;
+
+    #[test]
+    fn basic_tests() {
+        let mut time = Time::new();
+        time.assign(12, 30 , 0);
+
+        assert_eq!(time.secs_since_midnight(), 45000);
+
+        let mut second_time = Time::new();
+        second_time.assign(12, 30, 0);
+        assert_eq!(time.equals(second_time), true);
+    }
+
+    #[test]
+    fn inc_tests() {
+        let mut time = Time::new();
+        time.assign(0, 58, 59);
+
+        time.inc_hour();
+        time.inc_minute();
+        time.inc_second();
+        assert_eq!(time.secs_since_midnight(), 7200);
+    }
+
+    #[test]
+    fn string_tests() {
+        let mut time = Time::new();
+        time.assign(12, 0, 0);
+
+        assert_eq!(time.to_string(), "12:00:00 PM");
+        time.assign(0, 0, 0);
+        assert_eq!(time.to_string(), "12:00:00 AM");
+        assert_eq!(time.to_mil_string(), "00:00:00");
     }
 }
