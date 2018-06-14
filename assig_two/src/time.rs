89c8b731805ca6955b102f53d7082fb537703db1
Imogen Fulton <imogen@dfdt.me>
2018-06-14 15:13:43 +1000

    Julian's data structures are a pain

---
+++ assig_two/src/time.rs
@@ -0,0 +1,71 @@
+pub struct Time {
+    pub hour: i32,
+    pub minute: i32,
+    pub second: i32,
+}
+
+impl Time {
+    pub fn new() -> Self {
+        Time {
+            hour: 0,
+            minute: 0,
+            second: 0,
+        }
+    }
+
+    pub fn assign(&mut self, hour: i32, minute: i32, second: i32) {
+        self.hour = hour;
+        self.minute =  minute;
+        self.second = second;
+    }
+
+    pub fn incHour(&mut self) {
+        self.hour += 1;
+    }
+
+    pub fn incMinute(&mut self) {
+        self.minute += 1;
+
+        if self.minute == 60 {
+            self.minute = 0;
+            Time::incHour(self);
+        }
+    }
+
+    pub fn incSecond(&mut self) {
+        self.second += 1;
+
+        if self.second == 60 {
+            self.second = 0;
+            Time::incMinute(self);
+        }
+    }
+
+    pub fn toString(&mut self) -> String {
+        if self.hour >= 12 {
+            if self.hour == 12 {
+                return format!("{}:{}:{:} PM", 12, self.minute, self.second);
+            } else {
+                return format!("{}:{}:{} PM", self.hour - 12, self.minute, self.second);
+            }
+        } else {
+            if self.hour == 0 {
+                return format!("{}:{}:{} AM", 12, self.minute, self.second);
+            } else {
+                return format!("{}:{}:{} AM", self.hour, self.minute, self.second);
+            }
+        }
+    }
+
+    pub fn toMilString (&mut self) -> String {
+        return format!("{}:{}:{}", self.hour, self.minute, self.second);
+    }
+
+    pub fn secondsSinceMidnight(self) -> i32 {
+        self.second + (self.minute * 60) + (self.hour * 60 * 60)
+    }
+
+    pub fn equals(self, t: Time) -> bool {
+        self.secondsSinceMidnight() == t.secondsSinceMidnight()
+    }
+}
